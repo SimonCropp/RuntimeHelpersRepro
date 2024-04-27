19fd9e6e91205ef1858f7f8f80029f69d83f1f1d
Simon Cropp <simon.cropp@gmail.com>
2024-04-27 22:43:31 +1000

    .

--- ClassLibrary1/Usage.cs
+++ ClassLibrary1/Usage.cs
@@ -1,18 +1,7 @@
-﻿using NUnit.Framework;
-using NUnit.Framework.Legacy;
-
-[TestFixture]
-public class Usage
+﻿public class Usage
 {
-    [Test]
     public void Foo()
     {
-        var array = new[]
-        {
-            "value1",
-            "value2"
-        };
-        var subArray = array[..1];
-        ClassicAssert.AreEqual("value1", subArray[0]);
+        ReadOnlySpan<char> bar = ['d'];
     }
 }
