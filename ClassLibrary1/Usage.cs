2318ac735790f96ae7937552ccf17df2708268a5
Simon Cropp <simon.cropp@gmail.com>
2024-04-27 19:12:21 +1000

    .

--- ClassLibrary1/Usage.cs
+++ ClassLibrary1/Usage.cs
@@ -1,6 +1,11 @@
-﻿public class Usage
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+[TestFixture]
+public class Usage
 {
-    void Foo()
+    [Test]
+    public void Foo()
     {
         var array = new[]
         {
@@ -8,5 +13,6 @@
             "value2"
         };
         var subArray = array[..1];
+        ClassicAssert.AreEqual("value1", subArray[0]);
     }
 }
